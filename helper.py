7fbab5835a7bcf4f65c13229c61ead5083eac889
paulomarcos <paulomarcosdj@outlook.com>
2018-10-03 16:54:33 +0900

    Improving data quantity

--- helper.py
+++ helper.py
@@ -71,30 +71,31 @@ def gen_batch_function(data_folder, image_shape):
         :param batch_size: Batch Size
         :return: Batches of training data
         """
-        image_paths = glob(os.path.join(data_folder, 'image_2', '*.png'))
-        label_paths = {
-            re.sub(r'ground_truth', 'data', os.path.basename(path)): path
-            for path in glob(os.path.join(data_folder, 'gt_image_2', '*.png'))}
-        background_color = np.array([0, 0, 0, 0])
+        for folder in data_folder:
+            image_paths = glob(os.path.join(folder, 'data*.png'))
+            label_paths = {
+                re.sub(r'ground_truth', 'data', os.path.basename(path)): path
+                for path in glob(os.path.join(folder, 'ground_truth*.png'))}
+            background_color = np.array([0, 0, 0, 0])
 
-        random.shuffle(image_paths)
-        for batch_i in range(0, len(image_paths), batch_size):
-            images = []
-            gt_images = []
-            for image_file in image_paths[batch_i:batch_i+batch_size]:
-                gt_image_file = label_paths[os.path.basename(image_file)]
+            random.shuffle(image_paths)
+            for batch_i in range(0, len(image_paths), batch_size):
+                images = []
+                gt_images = []
+                for image_file in image_paths[batch_i:batch_i+batch_size]:
+                    gt_image_file = label_paths[os.path.basename(image_file)]
 
-                image = scipy.misc.imresize(scipy.misc.imread(image_file), image_shape)
-                gt_image = scipy.misc.imresize(scipy.misc.imread(gt_image_file), image_shape)
+                    image = scipy.misc.imresize(scipy.misc.imread(image_file), image_shape)
+                    gt_image = scipy.misc.imresize(scipy.misc.imread(gt_image_file), image_shape)
 
-                gt_bg = np.all(gt_image == background_color, axis=2)
-                gt_bg = gt_bg.reshape(*gt_bg.shape, 1)
-                gt_image = np.concatenate((gt_bg, np.invert(gt_bg)), axis=2)
+                    gt_bg = np.all(gt_image == background_color, axis=2)
+                    gt_bg = gt_bg.reshape(*gt_bg.shape, 1)
+                    gt_image = np.concatenate((gt_bg, np.invert(gt_bg)), axis=2)
 
-                images.append(image)
-                gt_images.append(gt_image)
+                    images.append(image)
+                    gt_images.append(gt_image)
 
-            yield np.array(images), np.array(gt_images)
+                yield np.array(images), np.array(gt_images)
     return get_batches_fn
 
 
